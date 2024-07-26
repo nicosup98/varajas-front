8a4ea7b4a08ecd4e6887c4224520fec405e9c90c
yonerwinr <ymrodriguez@urbe.edu.ve>
2024-07-26 08:59:55 -0400

    Update 1

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -11,11 +11,7 @@ function Root() {
    }, [])
    return (
       <>
-      <ul className="menu menu-horizontal bg-base-200">
-  <li><a>Item 1</a></li>
-  <li><a>Item 2</a></li>
-  <li><a>Item 5</a></li>
-</ul>
+     
 
          <div className="container mx-auto py-1">
             <Outlet />
