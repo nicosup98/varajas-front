f9ac22ef3ba22c0e8fce411dfa1f258fa7a7b2f3
nicosup <damiconicola98@gmail.com>
2024-07-26 14:13:27 -0400

    app route fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Root from "./routes/Root"
-import App from "./App"
+import App from "./routes/App"
 import './index.css'
 
 
