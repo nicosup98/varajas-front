316198eed7d8b945b0f557273be74ea0de0d0768
Yonerwin Rodriguez <142761855+yonerwinr@users.noreply.github.com>
2024-07-29 22:38:00 -0400

    Update App.jsx
    link de tipos de cartas

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -27,18 +27,17 @@ function App() {
 
   return (
     <div className="button-container h-[100vh]">
-
-      <Link to="/varajas/perro" className="large-button cursor-pointer" id="game1">
-      </Link>
-      <Link to="/varajas/pato" className="large-button cursor-pointer" id="game2">
-      </Link>
-      <Link to="/varajas/pokemon" className="large-button cursor-pointer" id="game3">
-      </Link>
-      <Link to="/varajas/anime" className="large-button cursor-pointer" id="game4">
-      </Link>
-      <Link to="/varajas/RYM" className="large-button cursor-pointer" id="game5">
-      </Link>
     </div>
+        <div className="button-container">
+          <div className="large-button" id="game1" onClick={() => handleNavigation('/varajas/perro')}></div>
+          <div className="large-button" id="game2" onClick={() => handleNavigation('//varajas/pato')}></div>
+          <div className="large-button" id="game3" onClick={() => handleNavigation('/varajas/pokemon')}></div>
+          <div className="large-button" id="game4" onClick={() => handleNavigation('/varajas/anime')}></div>
+          <div className="large-button" id="game5" onClick={() => handleNavigation('/varajas/rickymorty')}></div>
+        </div>
+      </div>
+    </div>
+  
   );
 }
 
